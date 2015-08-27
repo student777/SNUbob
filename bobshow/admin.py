a8a011a522e3a213fc6b8e30b658e44bfd0bfde8
student777 <darkdakku@gmail.com>
2015-08-28 01:11:19 +0900

    Revert "해커톤 슬슬 졸리다"
    This reverts commit 489ef72ce4f7524745a6c5b23dac61fb85a9ffd8.

--- bobshow/admin.py
+++ bobshow/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from bobshow.models import Bob, Place, Comment
 
-
-admin.site.register(Place)
-admin.site.register(Comment)
-admin.site.register(Bob)
+# Register your models here.
--- snubob/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from bobshow.models import Bob, Comment
-
-def index(request):
-    return render(request, "index.html", {
-
-        })
