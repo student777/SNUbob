75c2f806544544a07fd5506d03cce7c0c62dff76
student777 <darkdakku@gmail.com>
2015-08-31 01:35:30 +0900

    150831

---
+++ settings_prod.py
@@ -0,0 +1,18 @@
+import os
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'ubuntu',
+        'PASSWORD': '1234',
+        # 'HOST': '',
+        # 'PORT': '',
+    },
+}
+
+ALLOW_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
