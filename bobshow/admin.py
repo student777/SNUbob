942c3045506bffab59b2c3f84fb2065554bfff61
student777 <darkdakku@gmail.com>
2015-09-13 14:39:52 +0900

    admin 검색기능

--- bobshow/admin.py
+++ bobshow/admin.py
@@ -4,5 +4,10 @@ from bobshow.models import Bob, Place, Comment, Photo
 
 admin.site.register(Place)
 admin.site.register(Comment)
-admin.site.register(Bob)
 admin.site.register(Photo)
+
+
+@admin.register(Bob)
+class BobAdmin(admin.ModelAdmin):
+    list_display = ('name', 'place', 'content')
+    search_fields = ('name', 'place__name')
