9198680a986da66eb5601be62ea8203b7daaa320
student777 <darkdakku@gmail.com>
2015-09-18 16:28:32 +0900

    time+=9h

--- snubob/views.py
+++ snubob/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from bobshow.models import Bob
 import datetime
+from datetime import timedelta
 
 
 def index(request):
-    dat = datetime.date.today().isoformat()+'\n'
+    dt = datetime.date.today() + timedelta(hours=9)
+    dat = dt.isoformat()+'\n'
     bob_list = Bob.objects.filter(content__contains=dat).order_by('place')
     return render(request, "index.html", {'bob_list': bob_list, 'today': dat, })
