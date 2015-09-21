2dbba4115e14cb30cace10e082b0947275240631
student777 <darkdakku@gmail.com>
2015-09-22 01:51:00 +0900

    시간오류 수정

--- snubob/views.py
+++ snubob/views.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 
 
 def index(request):
-    dt = datetime.date.today() + timedelta(hours=9)
-    dat = dt.isoformat()+'\n'
+    dt = datetime.datetime.now() + timedelta(hours=9)
+    dat = dt.date().isoformat()+'\n'
     bob_list = Bob.objects.filter(content__contains=dat).order_by('place')
     return render(request, "index.html", {'bob_list': bob_list, 'today': dat, })
